3cd2f997a875ef11a2cdad87bfc024aaae7b1018
Claas Augner <caugner@mozilla.com>
2025-05-12 18:35:25 +0200

    fix(a11y-menu): add "Skip to search"

--- components/a11y-menu/server.js
+++ components/a11y-menu/server.js
@@ -7,9 +7,9 @@ export class A11yMenu extends ServerComponent {
    * @param {Fred.Context<Rari.BuiltPage>} context
    */
   render(context) {
-    // TODO Implement "Skip to search"
     return html`<ul class="a11y-menu">
       <li><a href="#content">${context.l10n`Skip to main content`}</a></li>
+      <li><a href="#search">${context.l10n`Skip to search`}</a></li>
     </ul>`;
   }
 }
