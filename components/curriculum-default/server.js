ac31d27b26803d8823ca5087a6dfe292db127cd9
Andi Pieper <apieper@mozilla.com>
2025-05-15 17:12:59 +0200

    scaffolding components

---
+++ components/curriculum-default/server.js
@@ -0,0 +1,21 @@
+import { html } from "lit";
+
+import { Section } from "../content/server.js";
+import { PageLayout } from "../page-layout/server.js";
+import { ServerComponent } from "../server/index.js";
+
+export class CurriculumDefault extends ServerComponent {
+  /**
+   * @param {Fred.Context<Rari.CurriculumPage>} context
+   */
+  render(context) {
+    console.log(context);
+    return PageLayout.render(
+      context,
+      html`
+        <h1>DEFAULT ${context.doc.title}</h1>
+        ${context.doc.body.map((section) => Section(context, section))}
+      `,
+    );
+  }
+}
