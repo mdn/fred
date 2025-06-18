1ff1b4e6a0a58ce8e236dd88fc393f89388f4149
Leo McArdle <leo@mca.is>
2025-06-18 16:19:46 +0000

    use proper node path

--- build/render.js
+++ build/render.js
@@ -1,15 +1,25 @@
 import { readFile } from "node:fs/promises";
 
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { render as distRender } from "../dist/ssr/index.js";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import("@rspack/core").StatsCompilation} */
-// TODO: use proper node path
 const clientManifest = JSON.parse(
-  await readFile("./dist/client/stats.json", "utf8"),
+  await readFile(
+    path.join(__dirname, "..", "dist", "client", "stats.json"),
+    "utf8",
+  ),
 );
 /** @type {import("@rspack/core").StatsCompilation} */
 const legacyManifest = JSON.parse(
-  await readFile("./dist/legacy/stats.json", "utf8"),
+  await readFile(
+    path.join(__dirname, "..", "dist", "legacy", "stats.json"),
+    "utf8",
+  ),
 );
 
 /**
