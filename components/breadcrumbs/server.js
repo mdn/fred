a872733944f312aeb66e8c79807581e28c356f79
Andi Pieper <apieper@mozilla.com>
2025-06-19 11:09:45 +0200

    Correct typing of the breadcrumbs component

--- components/breadcrumbs/server.js
+++ components/breadcrumbs/server.js
@@ -2,34 +2,69 @@ import { html, nothing } from "lit";
 
 import { ServerComponent } from "../server/index.js";
 
+// Some type wrangling to cover parents = context.doc.parents | no parents at all | doc.parents
+
+/**
+ * @typedef {import("@rari").Parent} Parent
+ */
+
+/**
+ * @typedef {object} DocParentsContext
+ * @property {string} renderer The renderer to use
+ * @property {{parents: Parent[]}} doc The document to render
+ */
+
+/**
+ * @typedef {object} DirectParentsContext
+ * @property {string} renderer The renderer to use
+ * @property {Parent[]} parents The parents to render
+ */
+
+/**
+ * @typedef {DocParentsContext | DirectParentsContext} BreadcrumbContext
+ */
+
+/**
+ * Type predicate to check if context has doc.parents
+ * @param {BreadcrumbContext} context
+ * @returns {context is DocParentsContext}
+ */
+function hasDocParents(context) {
+  return [
+    "Doc",
+    "CurriculumModule",
+    "CurriculumLanding",
+    "CurriculumOverview",
+    "CurriculumAbout",
+    "CurriculumDefault",
+  ].includes(context.renderer);
+}
+
+/**
+ * Type predicate to check if context has direct parents
+ * @param {BreadcrumbContext} context
+ * @returns {context is DirectParentsContext}
+ */
+function hasDirectParents(context) {
+  return [
+    "GenericDoc",
+    "SpaObservatoryLanding",
+    "SpaObservatoryAnalyze",
+    "GenericAbout",
+    "GenericCommunity",
+  ].includes(context.renderer);
+}
+
 export class Breadcrumbs extends ServerComponent {
   /**
-   * @param {import("@fred").Context} context
+   * @param {BreadcrumbContext} context
    */
   render(context) {
+    /** @type {Parent[]} */
     let parents;
-    if (
-      [
-        "Doc",
-        "CurriculumModule",
-        "CurriculumLanding",
-        "CurriculumOverview",
-        "CurriculumAbout",
-        "CurriculumDefault",
-      ].includes(context.renderer)
-    ) {
-      // @ts-expect-error
+    if (hasDocParents(context)) {
       parents = context.doc.parents;
-    } else if (
-      [
-        "GenericDoc",
-        "SpaObservatoryLanding",
-        "SpaObservatoryAnalyze",
-        "GenericAbout",
-        "GenericCommunity",
-      ].includes(context.renderer)
-    ) {
-      // @ts-expect-error
+    } else if (hasDirectParents(context)) {
       parents = context.parents;
     } else {
       return nothing;
@@ -38,7 +73,6 @@ export class Breadcrumbs extends ServerComponent {
     return html`
       <ul class="breadcrumbs">
         ${parents.map(
-          // @ts-expect-error
           ({ uri, title }) => html`
             <li>
               <a href=${uri}>${title}</a>
