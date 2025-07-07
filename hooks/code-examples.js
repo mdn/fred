e23b06d4cd313f816ce024d0dfa44e47cbd68b8b
Claas Augner <caugner@mozilla.com>
2025-07-07 23:27:31 +0200

    enhance(code-examples): don't require div.code-example wrapper

--- hooks/code-examples.js
+++ hooks/code-examples.js
@@ -1,5 +1,5 @@
 for (const pre of document.querySelectorAll(
-  `div.code-example pre:not(.hidden):not([class*="live-sample"]):not([class*="interactive-example"])`,
+  `pre:not(.hidden):not([class*="live-sample"]):not([class*="interactive-example"])`,
 )) {
   const { upgradePre } = await import("../components/code-example/element.js");
   upgradePre(pre);
