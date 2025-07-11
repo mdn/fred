ca4ec58cd8c55bde5fc02ce5f830f1d46528e3f3
Claas Augner <caugner@mozilla.com>
2025-07-11 12:51:35 +0200

    refactor(breadcrumbs-bar): extract _renderLanguageSwitcher()

--- components/breadcrumbs-bar/server.js
+++ components/breadcrumbs-bar/server.js
@@ -20,19 +20,6 @@ export class BreadcrumbsBar extends ServerComponent {
       ? html`<mdn-toggle-sidebar></mdn-toggle-sidebar>`
       : nothing;
 
-    /**
-     * @type {import("@rari").Translation[]}
-     */
-    const other_translations =
-      "other_translations" in context
-        ? context.other_translations
-        : "doc" in context && "other_translations" in context.doc
-          ? context.doc.other_translations
-          : [];
-    const native = other_translations.find(
-      (t) => t.locale === context.locale,
-    )?.native;
-
     return html`
       <div class="breadcrumbs-bar" data-scheme=${colorScheme}>
         ${toggleSidebar} ${Breadcrumbs.render(context)}
@@ -43,15 +30,34 @@ export class BreadcrumbsBar extends ServerComponent {
             ></mdn-collection-save-button>`
           : nothing}
         <mdn-color-theme></mdn-color-theme>
-        ${other_translations.length > 0
-          ? html`<mdn-language-switcher
-              locale=${context.locale}
-              native=${native}
-              translations=${JSON.stringify(other_translations)}
-              url=${context.url}
-            ></mdn-language-switcher>`
-          : nothing}
+        ${this._renderLanguageSwitcher(context)}
       </div>
     `;
+  }
+
+  /**
+   * @param {import("@fred").Context} context
+   */
+  _renderLanguageSwitcher(context) {
+    const other_translations =
+      "other_translations" in context
+        ? context.other_translations
+        : "doc" in context && "other_translations" in context.doc
+          ? context.doc.other_translations
+          : [];
+    const native = other_translations.find(
+      (t) => t.locale === context.locale,
+    )?.native;
+
+    if (!native) {
+      return nothing;
+    }
+
+    return html`<mdn-language-switcher
+      locale=${context.locale}
+      native=${native}
+      translations=${JSON.stringify(other_translations)}
+      url=${context.url}
+    ></mdn-language-switcher>`;
   }
 }
