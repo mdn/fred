b6a8dcf3a69bd7dea0b8b9dac153fb280231a2e6
Leo McArdle <leo@mca.is>
2025-07-15 18:03:49 +0000

    Revert "enhance(specifications-list): group by specification + collapse if necessary"
    This reverts commit dcb51346aac9eb34a3bf954976aada7f944f2ac5.

--- components/specifications-list/index.js
+++ components/specifications-list/index.js
@@ -1,5 +1,4 @@
 import { html, nothing } from "lit";
-import { join } from "lit/directives/join.js";
 
 /**
  * @param {import("@fred").Context} context
@@ -10,55 +9,17 @@ export function SpecificationsList(context, specifications) {
     return html`${context.l10n`This feature does not appear to be defined in any specification.`}`;
   }
 
-  /** @type {Map<string, string[]>} */
-  const urlsByTitle = new Map();
-  for (const { title, bcdSpecificationURL: url } of specifications) {
-    const urls = urlsByTitle.get(title) ?? [];
-    urls.push(url);
-    urlsByTitle.set(title, urls);
-  }
-
-  return html`<p>
-      ${context.l10n`This feature is defined in the following specifications`}:
-    </p>
-    ${specifications.length > 1
-      ? urlsByTitle.entries().map(([title, urls]) => {
-          return html`<details
-            class="specifications-list"
-            ?open=${specifications.length <= 3}
+  return html`${context.l10n`This feature is defined in the following specifications`}:
+    <ul>
+      ${specifications.map(({ title, bcdSpecificationURL: url }) => {
+        return html`<li>
+          <a class="external" href=${url} rel="noopener" target="_blank"
+            >${title}${url.includes("#")
+              ? html`<br />
+                  <small># ${url.split("#")[1]}</small>`
+              : nothing}</a
           >
-            <summary>${title}</summary>
-            <ul>
-              ${urls.map((url) => html`<li>${SpecificationLink(url)}</li>`)}
-            </ul>
-          </details>`;
-        })
-      : specifications[0]
-        ? html`<ul class="specifications-list">
-            <li>
-              ${SpecificationLink(
-                specifications[0].bcdSpecificationURL,
-                specifications[0].title,
-              )}
-            </li>
-          </ul>`
-        : nothing}`;
-}
-
-/**
- * @param {string} url
- * @param {string} [title]
- */
-function SpecificationLink(url, title) {
-  const hash = url.split("#")[1];
-
-  const label = [
-    title && html`${title}`,
-    title && hash && html`<br />`,
-    hash && html`# ${hash}`,
-  ].filter(Boolean);
-
-  return html`<a class="external" href=${url} rel="noopener" target="_blank"
-    >${join(label, "")}</a
-  >`;
+        </li>`;
+      })}
+    </ul>`;
 }
