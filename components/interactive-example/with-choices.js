03b4151cc34a84c40cee856039a56bc153b81e40
Claas Augner <caugner@mozilla.com>
2025-08-13 15:39:24 +0200

    fix(interactive-example): disable Reset until choice update

--- components/interactive-example/with-choices.js
+++ components/interactive-example/with-choices.js
@@ -23,6 +23,7 @@ export const InteractiveExampleWithChoices = (Base) =>
     static properties = {
       __choiceSelected: { state: true },
       __choiceUnsupported: { state: true },
+      __choiceUpdated: { state: true },
     };
 
     /** @param {any[]} _args  */
@@ -32,6 +33,8 @@ export const InteractiveExampleWithChoices = (Base) =>
       this.__choiceSelected = -1;
       /** @type {boolean[]} */
       this.__choiceUnsupported = [];
+      /** @type {boolean} */
+      this.__choiceUpdated = false;
     }
 
     /** @param {MouseEvent} event  */
@@ -56,11 +59,13 @@ export const InteractiveExampleWithChoices = (Base) =>
         if (this.__choiceSelected === this.#getIndex(target)) {
           this.#selectChoice(target);
         }
+        this.__choiceUpdated = true;
       }
     }
 
     #resetChoices() {
       this.__choiceSelected = -1;
+      this.__choiceUpdated = false;
 
       const editorNodes = [
         ...(this.shadowRoot?.querySelectorAll("mdn-play-editor") || []),
@@ -108,7 +113,11 @@ export const InteractiveExampleWithChoices = (Base) =>
         <div class="template-choices">
           <header>
             <h4>${decode(this.name)}</h4>
-            <mdn-button id="reset" @click=${this._reset} variant="secondary"
+            <mdn-button
+              id="reset"
+              @click=${this._reset}
+              variant="secondary"
+              .disabled=${!this.__choiceUpdated}
               >${this.l10n`Reset`}</mdn-button
             >
           </header>
