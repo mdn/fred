cd238141c231dad745351287fb443c6dfb83ebcc
Claas Augner <caugner@mozilla.com>
2025-08-14 10:10:47 +0200

    fix(site-search): add h1
    Same look as h1 on "Page not found" page.

--- components/site-search/server.js
+++ components/site-search/server.js
@@ -11,6 +11,7 @@ export class SiteSearch extends ServerComponent {
     return PageLayout.render(
       context,
       html`<div class="site-search">
+        <h1>${context.l10n`Search`}</h1>
         <mdn-site-search></mdn-site-search>
       </div>`,
     );
