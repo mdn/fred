c163acdbda8e7a2711c1820a705a94f064f51f0d
Brian Smith <brian@smith.berlin>
2025-08-19 10:05:41 +0200

    chore: Use 'getting started' docs on MDN instead of GitHub READMEs

--- components/article-footer/server.js
+++ components/article-footer/server.js
@@ -36,11 +36,10 @@ export class ArticleFooter extends ServerComponent {
  */
 function Contribute(context) {
   return html`<a
-    class="article-footer__contribute external"
-    href="https://github.com/mdn/content/blob/main/CONTRIBUTING.md"
-    title=${context.l10n`This will take you to our contribution guidelines on GitHub.`}
+    class="article-footer__contribute"
+    href="/en-US/docs/MDN/Community/Getting_started"
+    title=${context.l10n`This will take you to our contribution documentation.`}
     target="_blank"
-    rel="noopener noreferrer"
     >${context.l10n`Learn how to contribute`}</a
   >`;
 }
