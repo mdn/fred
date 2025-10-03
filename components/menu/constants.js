0253dbe528335f2ac61fb689c876dc3c9ad4a5c6
Claas Augner <caugner@mozilla.com>
2025-10-03 17:18:51 +0200

    chore(menu): turn check into update script

--- components/menu/constants.js
+++ components/menu/constants.js
@@ -434,3 +434,14 @@ export const TABS = [
  */
 /** @type {Record<string, string[]>} */
 export const MISSING_DOCS = Object.freeze(missingDocs);
+
+export const TRANSLATED_LOCALES = [
+  "es",
+  "fr",
+  "ja",
+  "ko",
+  "pt-BR",
+  "ru",
+  "zh-CN",
+  "zh-TW",
+];
