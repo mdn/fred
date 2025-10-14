fba327f0cae1911b18ecd21c8d780280daa06cdc
Claas Augner <495429+caugner@users.noreply.github.com>
2025-10-14 15:32:22 +0200

    Update userIsOptedOut JSDoc comment

--- utils/telemetry-opt-out.js
+++ utils/telemetry-opt-out.js
@@ -2,7 +2,7 @@ const FIRST_PARTY_DATA_OPT_OUT_COOKIE_NAME = "moz-1st-party-data-opt-out";
 
 /**
  * Checks if user has opted out of first-party data collection.
- * @param {string} [cookie] - Cookie value to check (for testing)
+ * @param {string} [cookie] - An optional mock cookie string to ease unit testing.
  * @returns {boolean} True if user has opted out, false otherwise
  */
 export function userIsOptedOut(cookie) {
