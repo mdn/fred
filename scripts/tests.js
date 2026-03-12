53ec397ed3c70838d68bb5d6a390eb3e2037e920
Leo McArdle <leo@mozilla.com>
2026-03-12 11:41:54 +0000

    chore: move wdio tests and support infra behind flags

--- scripts/tests.js
+++ scripts/tests.js
@@ -4,19 +4,41 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const argv = await yargs(hideBin(process.argv))
+  .option("e2e", {
+    describe: "run e2e tests",
+    type: "boolean",
+    default: false,
+  })
+  .option("rari", {
+    describe: "run rari",
+    type: "boolean",
+    default: false,
+  })
+  .option("preview", {
+    describe: "run fred preview",
+    type: "boolean",
+    default: false,
+  })
   .option("content", {
-    describe: "if specified, run fred from the content repo located here",
+    describe: "run fred and rari from this content repo",
     type: "string",
+  })
+  .conflicts({
+    content: ["rari", "preview"],
   })
   .parse();
 
 concurrently(
   [
-    {
-      command: `npm run wdio`,
-      name: "wdio",
-      prefixColor: "green",
-    },
+    ...(argv.e2e
+      ? [
+          {
+            command: `npm run wdio`,
+            name: "wdio",
+            prefixColor: "green",
+          },
+        ]
+      : []),
     ...(argv.content
       ? [
           {
@@ -26,18 +48,25 @@ concurrently(
             prefixColor: "blue",
           },
         ]
-      : [
+      : []),
+    ...(argv.preview
+      ? [
           {
             command: `npm run preview`,
             name: "server",
             prefixColor: "red",
           },
+        ]
+      : []),
+    ...(argv.rari
+      ? [
           {
             command: `"${rariBin}" serve`,
             name: "rari",
             prefixColor: "blue",
           },
-        ]),
+        ]
+      : []),
   ],
   {
     killOthersOn: ["failure", "success"],
