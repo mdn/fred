b74e094514f97166e300a9992b3de2cd11f55ea8
Leo McArdle <leo@mozilla.com>
2026-03-12 11:22:09 +0000

    chore: merge npm-test.js file into tests.js file, use yargs

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,5 +1,14 @@
 import { rariBin } from "@mdn/rari";
 import { concurrently } from "concurrently";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = await yargs(hideBin(process.argv))
+  .option("content", {
+    describe: "if specified, run fred from the content repo located here",
+    type: "string",
+  })
+  .parse();
 
 concurrently(
   [
@@ -8,16 +17,27 @@ concurrently(
       name: "wdio",
       prefixColor: "green",
     },
-    {
-      command: `npm run preview`,
-      name: "server",
-      prefixColor: "red",
-    },
-    {
-      command: `"${rariBin}" serve`,
-      name: "rari",
-      prefixColor: "blue",
-    },
+    ...(argv.content
+      ? [
+          {
+            cwd: argv.content,
+            command: `npm start`,
+            name: "content",
+            prefixColor: "blue",
+          },
+        ]
+      : [
+          {
+            command: `npm run preview`,
+            name: "server",
+            prefixColor: "red",
+          },
+          {
+            command: `"${rariBin}" serve`,
+            name: "rari",
+            prefixColor: "blue",
+          },
+        ]),
   ],
   {
     killOthersOn: ["failure", "success"],
