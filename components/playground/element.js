e00f75c846e727dd1307625b5597873afe9b7ff1
Claas Augner <caugner@mozilla.com>
2026-04-03 14:32:37 +0200

    chore(playground): revert reset handler

--- components/playground/element.js
+++ components/playground/element.js
@@ -95,19 +95,18 @@ export class MDNPlayground extends L10nMixin(LitElement) {
   }
 
   _reset() {
+    const controller = this._controller.value;
     if (
       confirm(
         this.l10n(
           "playground-do-you-really-want-to-revert-you",
         )`Do you really want to revert your changes?`,
-      )
+      ) &&
+      controller
     ) {
-      const controller = this._controller.value;
-      if (controller) {
-        controller.reset();
-        this._storeSession();
-        this.requestUpdate();
-      }
+      controller.reset();
+      this._storeSession();
+      this.requestUpdate();
     }
   }
 
