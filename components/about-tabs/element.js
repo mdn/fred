eb89ef5d26d91296db423756607417ced9923c7e
Claas Augner <caugner@mozilla.com>
2026-04-03 11:27:06 +0200

    chore(about-tabs): measure via data-glean-id

--- components/about-tabs/element.js
+++ components/about-tabs/element.js
@@ -1,7 +1,5 @@
 import { LitElement, html } from "lit";
 
-import { gleanClick } from "../../utils/glean.js";
-
 import styles from "./element.css?lit";
 
 export class MDNAboutTabs extends LitElement {
@@ -88,7 +86,6 @@ export class MDNAboutTabs extends LitElement {
         e.preventDefault();
         if (tabEl.dataset.panelId) {
           globalThis.location.hash = tabEl.dataset.panelId;
-          gleanClick(`about: tab -> ${tabEl.dataset.panelId}`);
         }
         this.active_index = i;
 
