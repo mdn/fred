82e1a6f1ca2a065dab33f3ca0109261d5fa0b7fa
Claas Augner <caugner@mozilla.com>
2026-04-13 14:22:44 +0200

    chore(utils): remove `randomIdString()`

--- components/utils/index.js
+++ components/utils/index.js
@@ -29,14 +29,3 @@ export function createElementId(content, prefix = "id-") {
   }
   return `${prefix}${hashString(String(content))}`;
 }
-
-/**
- * @deprecated Use deterministicIdString instead
- * Used to generate a random element id by combining a prefix with a random string.
- *
- * @param {string} prefix
- * @returns {string}
- */
-export function randomIdString(prefix = "id-") {
-  return Math.random().toString(36).replace("0.", prefix);
-}
