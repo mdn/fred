f7e28cabbefb56568b827f2e0893715179b59d34
Leo McArdle <leo@mozilla.com>
2026-04-21 15:03:49 +0100

    feat(bcd): render links to JSON in simplified HTML

---
+++ components/bcd-section/server.js
@@ -0,0 +1,48 @@
+import { html } from "@lit-labs/ssr";
+import { nothing } from "lit";
+import { ifDefined } from "lit/directives/if-defined.js";
+
+import { BCD_BASE_URL } from "../env/index.js";
+import { HeadingAnchor } from "../heading-anchor/server.js";
+import { ServerComponent } from "../server/index.js";
+
+export class BCDSection extends ServerComponent {
+  /**
+   * @param {import("@fred").Context} _context
+   * @param {import("@rari").Compat} section
+   */
+  render(_context, { id, title, query, isH3 }) {
+    const level = isH3 ? 3 : 2;
+    return html`<section
+      class="content-section"
+      aria-labelledby=${ifDefined(id ?? undefined)}
+    >
+      ${HeadingAnchor.render(level, id ? String(id) : null, String(title))}
+      <mdn-compat-table-lazy
+        locale="en-US"
+        query=${query}
+      ></mdn-compat-table-lazy>
+    </section>`;
+  }
+
+  /**
+   * @param {import("@fred").Context} _context
+   * @param {import("@rari").Compat} section
+   */
+  renderSimplified(_context, { id, title, query, isH3 }) {
+    return html`<section
+      class="content-section"
+      aria-labelledby=${ifDefined(id ?? undefined)}
+    >
+      ${isH3
+        ? nothing
+        : HeadingAnchor.render(2, id ? String(id) : null, String(title))}
+      <a
+        href="${BCD_BASE_URL}/bcd/api/v0/current/${query}.json"
+        target="_blank"
+        rel="noopener"
+        ><code>${query}</code></a
+      >
+    </section>`;
+  }
+}
--- components/content-section/server.js
+++ components/content-section/server.js
@@ -2,6 +2,7 @@ import { html } from "@lit-labs/ssr";
 import { ifDefined } from "lit/directives/if-defined.js";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 
+import { BCDSection } from "../bcd-section/server.js";
 import { HeadingAnchor } from "../heading-anchor/server.js";
 import { ServerComponent } from "../server/index.js";
 import { SpecificationsList } from "../specifications-list/index.js";
@@ -10,12 +11,11 @@ export class ContentSection extends ServerComponent {
   /**
    * @param {import("@fred").Context} context
    * @param {import("@rari").Section} section
-   * @returns {import("@lit").TemplateResult}
    */
   render(context, { type, value }) {
     switch (type) {
       case "browser_compatibility": {
-        return BCD(value);
+        return BCDSection.render(context, value);
       }
       case "specifications": {
         return SpecificationsSection(context, value);
@@ -40,24 +40,6 @@ function Prose({ id, title, content, isH3 }) {
   >
     ${HeadingAnchor.render(level, id ? String(id) : null, String(title))}
     ${unsafeHTML(content)}
-  </section>`;
-}
-
-/**
- * @param {import("@rari").Compat} section
- * @returns {import("@lit").TemplateResult}
- */
-function BCD({ id, title, query, isH3 }) {
-  const level = isH3 ? 3 : 2;
-  return html`<section
-    class="content-section"
-    aria-labelledby=${ifDefined(id ?? undefined)}
-  >
-    ${HeadingAnchor.render(level, id ? String(id) : null, String(title))}
-    <mdn-compat-table-lazy
-      locale="en-US"
-      query=${query}
-    ></mdn-compat-table-lazy>
   </section>`;
 }
 
