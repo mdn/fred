14109b42338e6b432be5d462ba31e76f38143049
Claas Augner <caugner@mozilla.com>
2026-04-28 22:41:44 +0200

    chore(vendor/yari): drop unused RenderSideBar export and leaf helpers
    RenderSideBar (and its SidebarLeaf/SidebarLeaflets internals) had no
    caller; only SidebarContainer is imported (by static-page).

--- vendor/yari/client/src/document/organisms/sidebar/index.tsx
+++ vendor/yari/client/src/document/organisms/sidebar/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Button } from "../../../ui/atoms/button";
 
@@ -81,82 +80,5 @@ export function SidebarContainer({
         </nav>
       </aside>
     </>
-  );
-}
-
-export function RenderSideBar({ doc }) {
-  if (!doc.related_content) {
-    return (
-      <SidebarContainer doc={doc} label="Related Topics">
-        {doc.sidebarHTML && (
-          <>
-            <div
-              className="sidebar-body"
-              dangerouslySetInnerHTML={{
-                __html: `${doc.sidebarHTML}`,
-              }}
-            />
-          </>
-        )}
-      </SidebarContainer>
-    );
-  }
-  return doc.related_content.map((node) => (
-    <SidebarLeaf key={node.title} parent={node} doc={doc} />
-  ));
-}
-
-function SidebarLeaf({ doc, parent }) {
-  return (
-    <SidebarContainer doc={doc}>
-      <h4 className="sidebar-heading">{parent.title}</h4>
-      <ul>
-        {parent.content.map((node) => {
-          if (node.content) {
-            return (
-              <li key={node.title}>
-                <SidebarLeaflets node={node} />
-              </li>
-            );
-          } else {
-            return (
-              <li key={node.uri}>
-                <Link to={node.uri}>{node.title}</Link>
-              </li>
-            );
-          }
-        })}
-      </ul>
-    </SidebarContainer>
-  );
-}
-
-function SidebarLeaflets({ node }) {
-  return (
-    <details open={node.open}>
-      <summary>
-        {node.uri ? <Link to={node.uri}>{node.title}</Link> : node.title}
-      </summary>
-      <ol>
-        {node.content.map((childNode) => {
-          if (childNode.content) {
-            return (
-              <li key={childNode.title}>
-                <SidebarLeaflets node={childNode} />
-              </li>
-            );
-          } else {
-            return (
-              <li
-                key={childNode.uri}
-                className={childNode.isActive && "active"}
-              >
-                <Link to={childNode.uri}>{childNode.title}</Link>
-              </li>
-            );
-          }
-        })}
-      </ol>
-    </details>
   );
 }
