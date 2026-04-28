3048afa442618cd8ef8e5f099a6e1077622d8859
Claas Augner <caugner@mozilla.com>
2026-04-28 20:16:43 +0200

    chore(vendor/yari): remove yari's CRA-style client build pipeline

--- vendor/yari/client/jest-setup.ts
+++
@@ -1,8 +0,0 @@
-//globalThis.IS_REACT_ACT_ENVIRONMENT = true;
-
-if (!globalThis.TextEncoder || !globalThis.TextDecoder) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { TextDecoder, TextEncoder } = require("node:util");
-  globalThis.TextEncoder = TextEncoder;
-  globalThis.TextDecoder = TextDecoder;
-}
