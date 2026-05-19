26c4fa72b77d3d94d0fc1258638bea820c783a9a
Leo McArdle <leo@mozilla.com>
2026-05-19 17:09:46 +0100

    chore: add visual-report command

---
+++ scripts/visual-report.js
@@ -0,0 +1,47 @@
+import { execSync } from "node:child_process";
+import path from "node:path";
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const FRED_ROOT = path.join(import.meta.dirname, "..");
+
+await yargs(hideBin(process.argv))
+  .command(
+    "generate",
+    "generate report",
+    (yargs) =>
+      yargs.option("serve", {
+        describe: "serve report after generation",
+        type: "boolean",
+        default: false,
+      }),
+    async (argv) => {
+      const jsonOutput = path.join(
+        FRED_ROOT,
+        "test",
+        "tmp",
+        "actual",
+        "output.json",
+      );
+      const reportFolder = path.join(FRED_ROOT, "test", "tmp");
+      execSync(
+        `npx wdio-visual-reporter --jsonOutput=${jsonOutput} --reportFolder=${reportFolder}`,
+        { stdio: "inherit" },
+      );
+      if (argv.serve) await serve();
+    },
+  )
+  .command("serve", "serve report", () => {}, serve)
+  .demandCommand(1)
+  .parse();
+
+async function serve() {
+  const { default: express } = await import("express");
+  const WDIO_PORT = process.env.WDIO_PORT || "3002";
+  const app = express();
+  app.use("/", express.static(path.join(FRED_ROOT, "test", "tmp", "report")));
+  app.listen(WDIO_PORT, () => {
+    console.log(`WebdriverIO visual reporter started on port ${WDIO_PORT}`);
+  });
+}
