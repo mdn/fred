f794093fe2e904f7edfc75a310a3418850fe3ea8
Leo McArdle <leo@mozilla.com>
2026-08-21 10:00:59 +0100

    chore(sandbox): pass context to sandbox components (#1819)

--- components/sandbox/class.js
+++ components/sandbox/class.js
@@ -2,19 +2,21 @@ export class SandboxComponent {
   /**
    * @template {typeof SandboxComponent} T
    * @this {T}
+   * @param {import("@fred").Context} context
    * @returns {ReturnType<InstanceType<T>["render"]>}
    */
-  static render() {
-    return new this().render();
+  static render(context) {
+    return new this().render(context);
   }
 
   /* eslint-disable jsdoc/reject-any-type -- abstract render is overridden by subclasses (needs a supertype) yet consumed via generic `ReturnType<...>` (needs `any`); neither `unknown` nor `never` satisfies both */
   /**
    * @abstract
+   * @param {import("@fred").Context} _context
    * @returns {any}
    */
   /* eslint-enable jsdoc/reject-any-type */
-  render() {
+  render(_context) {
     throw new Error("Must be implemented by subclass");
   }
 }
