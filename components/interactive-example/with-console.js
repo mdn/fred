bbd2725d08c9dfc6113f895103b10b7c05da4e30
Claas Augner <caugner@mozilla.com>
2026-09-17 19:32:11 +0200

    refactor(interactive-example): use fixed heading IDs

--- components/interactive-example/with-console.js
+++ components/interactive-example/with-console.js
@@ -12,7 +12,6 @@ import "../ix-tab/element.js";
 import "../ix-tab-panel/element.js";
 import "../ix-tab-wrapper/element.js";
 import { L10nMixin } from "../../l10n/mixin.js";
-import { randomIdString } from "../utils/index.js";
 
 /**
  * @import { InteractiveExampleBase } from "./element.js";
@@ -27,7 +26,7 @@ import { randomIdString } from "../utils/index.js";
 export const InteractiveExampleWithConsole = (Base) =>
   class extends L10nMixin(Base) {
     #render() {
-      const id = randomIdString();
+      const id = "ix-console";
 
       return html`
         <mdn-play-controller ${ref(this._controller)}>
